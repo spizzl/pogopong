4f1c5ec6cd714018366b46b53e1a4fc2c504b04a
holzbauleiste <florinwoeginger@googlemail.com>
2020-04-01 15:34:28 +0200

    jo

---
+++ server.js
@@ -0,0 +1,20 @@
+
+'use strict';
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 5000;
+const INDEX = '/public/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`lausche auf ${PORT}`));
+
+const zocket = socketIO(server);
+
+zocket.on('connection', (socket) => {
+  console.log('Atze ist da');
+  socket.on('disconnect', () => console.log('Pole'));
+});
+
+setInterval(() => zocket.emit('tacho', new Date().toTimeString()), 1000);
