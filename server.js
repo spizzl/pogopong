9bef67d6065a7840b6a9007f868ef2967af899bb
holzbauleiste <florinwoeginger@googlemail.com>
2020-04-02 03:30:47 +0200

    mans not hot

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-
 'use strict';
 const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 const INDEX = '/public/test.html';
+var app = express()
+var server = app.listen(PORT);
 
+
+app.use(express.static('public'));
+
+function clog(msg) {
+    console.log(msg);
+}
+/*
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`lausche auf ${PORT}`));
-
+*/
 const zocket = socketIO(server);
-var online = 0
-
 
 zocket.on('connection', (socket) => {
   console.log('Atze ist da');
-  online += 1
-  socket.on('disconnect', () => online -= 1);
+  socket.on('disconnect', () => console.log('Pole'));
 });
 
-setInterval(() => zocket.emit('status', online), 1000);
+setInterval(() => zocket.emit('tacho', new Date().toTimeString()), 1000);
