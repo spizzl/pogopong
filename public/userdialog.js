3301ba9cc1cd1e5117adc4588b52e9dfe46807e5
holzbauleiste <florinwoeginger@googlemail.com>
2020-04-03 14:08:16 +0200

    indexhtml edit

--- public/userdialog.js
+++ public/userdialog.js
@@ -1,13 +1,21 @@
+class UserdDialog {
 
-function opendialog(){
+  constructor() {
+    this.background = document.getElementsByClassName('underpopup')[0]
+    this.mainpopup = document.getElementsByClassName('underpopup')[0]
+    this.notifications = document.getElementsByClassName('underpopup')[0]
+  }
 
 
-  document.getElementsByClassName('popup')[0].style.display = "block";
-  document.getElementsByClassName('underpopup')[0].style.opacity = 0.4;
+  function opendialog(dialog, html){
+    document.getElementsByClassName('popup')[0].style.display = "block";
+    .style.opacity = 0.4;
 
-}
+  }
 
-function closedialog(){
-  document.getElementsByClassName('popup')[0].style.display = "none";
-  document.getElementsByClassName('underpopup')[0].style.opacity = 1;
+  function closedialog(){
+    document.getElementsByClassName('popup')[0].style.display = "none";
+    document.getElementsByClassName('underpopup')[0].style.opacity = 1;
+  }
+
 }
